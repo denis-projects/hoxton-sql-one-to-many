efecb24a57417ffdc546648859c93fd4a2cc2562
besmaerteralb <denis.mimini.1@outlook.com>
2022-02-22 19:32:41 +0100

    .

--- index.ts
+++ index.ts
@@ -10,111 +10,127 @@ const db = new Database('./data.db', {
     verbose: console.log
 })
 
-//Get details of museums
-//Get details of works
 
 
-const createMuseum = db.prepare(`
-INSERT INTO museums (name, city) VALUES (?, ?);
-`)
+//Get museums and works by museum id
 
-const createWork = db.prepare(`
-INSERT INTO works (name, picture, workId) VALUES (?, ?, ?);
-`)
+const getWorksByMuseumId = db.prepare(`SELECT * FROM works WHERE museumId = ?;`)
+
+const getAllMuseums = db.prepare(`SELECT * FROM museums`)
+
+app.get(`/museums`, (req, res) => {
+    const museums = getAllMuseums.all()
+
+    for (const museum of museums) {
+        const works = getWorksByMuseumId.all(museum.id)
+        museum.works = works
+    }
+    res.send(museums)
+})
 
 
-const getAllMuseums = db.prepare(`
-SELECT * FROM museums;
-`)
+
+//Get all works
+
+const getAllWorks = db.prepare(`SELECT * FROM  works;`)
+
+const getWorksById = db.prepare(`SELECT * FROM works WHERE id = ?`)
+
+app.get(`/works`, (req, res) => {
+    //get all works
+    const works = getAllWorks.all()
+    // for each work add museum
+    for (const work of works) {
+        const museum = getWorksById.get(work.museumId)
+        work.museum = museum
+    }
+
+    res.send(works)
+})
+
+
 
 const getMuseumById = db.prepare(`
 SELECT * FROM museums WHERE id = ?
 `)
 
-const getMuseumByWorkId = db.prepare(`
-SELECT * FROM works WHERE workId = ?
-`)
 
-
-app.get(`/museums`, (req, res) => {
-    const result = getAllMuseums.all()
-
-    res.send(result)
-})
 
 app.get(`/museums/:id`, (req, res) => {
     const id = req.params.id
 
-    const result = getMuseumById.get(id)
+    const museum = getMuseumById.get(id)
+    res.send(museum)
 
-    if (result) {
-        const museums = getMuseumByWorkId.all(result.id)
-        result.id = museums
-        res.send(result)
+    if (museum) {
+        const works = getMuseumById.all(museum.id)
+        museum.works = works
+        res.send(museum)
     } else {
         res.status(404).send({ error: ' Museum not found' })
     }
 
 })
 
-
-
-const getAllWorks = db.prepare(`
-SELECT * FROM works;
-`)
-
-const getWorksById = db.prepare(`
-SELECT * FROM works WHERE id = ?
-`)
-
-app.get(`/works`, (req, res) => {
-    const result = getAllWorks.all()
-
-    for (const work of result) {
-        const museum = getMuseumById.get(work.WorkId)
-        work.museum = museum
-    }
-    res.send(result)
-})
-
 app.get(`/works/:id`, (req, res) => {
     const id = req.params.id
 
-    const result = getWorksById.get(id)
+    const work = getWorksById.get(id)
 
-    res.send(result)
-})
-
-//Create a new museum
-
-app.post(`/museums`, (req, res) => {
-    const { name, city } = req.body
-
-    const result = createMuseum.run(name, city)
-
-    if (result.changes !== 0) {
-        const museum = getMuseumById.get(result.lastInsertRowid)
-        res.send(museum)
-    } else {
-        res.status(404).send({ error: ' somethig went wrong' })
-    }
-
-})
-
-
-//Cerate a new work
-
-app.post(`/works`, (req, res) => {
-    const { name, picture, workId } = req.body
-
-    const result = createWork.run(name, picture, workId)
-
-    if (result.changes !== 0) {
-        const work = getWorksById.get(result.lastInsertRowid)
+    if (work) {
+        const museum = getWorksById.get(work.museumId)
+        work.museum = museum
         res.send(work)
+
     } else {
-        res.status(404).send({ error: 'something went wrong' })
+        res.status(404).send({ error: ' Work not found' })
     }
+
 })
 
 app.listen(1234, () => console.log(`Listening on : http://localhost:1234`))
+
+
+
+
+
+
+// //Create a new museum or work
+
+// const createMuseum = db.prepare(`
+// INSERT INTO museums (name, city) VALUES (?, ?);
+// `)
+
+// const createWork = db.prepare(`
+// INSERT INTO works (name, picture, workId) VALUES (?, ?, ?);
+// `)
+
+
+// app.post(`/museums`, (req, res) => {
+//     const { name, city } = req.body
+
+//     const result = createMuseum.run(name, city)
+
+//     if (result.changes !== 0) {
+//         const museum = getMuseumById.get(result.lastInsertRowid)
+//         res.send(museum)
+//     } else {
+//         res.status(404).send({ error: ' somethig went wrong' })
+//     }
+
+// })
+
+
+// app.post(`/works`, (req, res) => {
+//     const { name, picture, workId } = req.body
+
+//     const result = createWork.run(name, picture, workId)
+
+//     if (result.changes !== 0) {
+//         const work = getWorksById.get(result.lastInsertRowid)
+//         res.send(work)
+//     } else {
+//         res.status(404).send({ error: 'something went wrong' })
+//     }
+// })
+
